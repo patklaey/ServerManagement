988229046fd046a0d7f072dcdf5995e480e24264
tgdklpa4 <pat.klaey@swisscom.com>
2016-11-17 12:09:37 +0100

    Extending monitoring script to send mails if needed

--- monitoring/monitoring.pl
+++ monitoring/monitoring.pl
@@ -1,14 +1,20 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
+use lib '/root/scripts/utils';
+use Mail;
 
 my @load = getCpuUsage();
 if ($load[0] > 1 || $load[1] > 0.75 || $load[2] > 0.5)
 {
-    print "oups, we have high load: @load";
-} else
-{
-    print "Everything OK, we have low load: @load";
+    my $mailer = Mail->new();
+    my $message = `top -n 1 -b`;
+    $message = "Subject: Alert on patklaey.ch\nHello, there seems to be a problem on the server patklaey.ch:\n\n" . $message;
+    $mailer->send($message);
+    if ( $mailer->error() )
+    {
+        print "Problem, sending mail failed:". $mailer->error()."\n";
+    }
 }
 
 sub getCpuUsage
