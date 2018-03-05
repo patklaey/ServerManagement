c1401a5b843ddcfc49986c1885b7fe7a10da6fbd
Kläy Pat <pat.klaey@swisscom.com>
2018-03-05 08:44:59 +0100

    Putting tar directly under correct folder

--- backup/logrotate.sh
+++ backup/logrotate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 folderName=`date +'%b-%Y'`
 
-tar -zcvf ${folderName}-schedule.tar.gz /root/idrive/Backup/Scheduled/LOGS/*
-tar -zcvf ${folderName}-manual.tar.gz /root/idrive/Backup/Manual/LOGS/*
+tar -zcvf /var/log/idrive/${folderName}-schedule.tar.gz /root/idrive/Backup/Scheduled/LOGS/*
+tar -zcvf /var/log/idrive/${folderName}-manual.tar.gz /root/idrive/Backup/Manual/LOGS/*
 
 rm /root/idrive/Backup/Scheduled/LOGS/*
 rm /root/idrive/Backup/Manual/LOGS/*
