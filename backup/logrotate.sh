c749bdf12a1f8cdc081b6b5606ac6e26a16e86fe
Pat Kläy <pat@247.ch>
2020-05-30 11:57:22 +0200

    typo

--- backup/logrotate.sh
+++ backup/logrotate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 folderName=`date +'%b-%Y'`
 
-tar -zcvf /var/log/idrive/${folderName}-schedule.tar.gz /var/log/idrive/*_Scheduled
+tar -zcvf /var/log/idrive/${folderName}-scheduled.tar.gz /var/log/idrive/*_Scheduled
 tar -zcvf /var/log/idrive/${folderName}-manual.tar.gz /var/log/idrive/*_Manual
 
 rm /var/log/idrive/*_Scheduled
